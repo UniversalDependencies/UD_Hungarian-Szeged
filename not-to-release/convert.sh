d44ffdcd01f0317650933895de9da02ac2326675
Martin Popel <popel@ufal.mff.cuni.cz>
2017-02-21 18:59:52 +0100

    conversion to UDv2 guidelines
    Now it passes all the validation.py tests.
    The maintainers of UD_Hungarian should check all the nodes marked with ToDo.

---
+++ not-to-release/convert.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# hu-specific transformation: Aspect=Freq -> Aspect=Iter,
+# see http://universaldependencies.org/u/feat/Aspect.html#Iter
+
+for a in train dev test; do
+    cat hu-ud-$a.conllu | udapy -s ud.SetSpaceAfter ud.Convert1to2 util.Eval \
+        tree="tree.bundle.bundle_id='$a-'+tree.bundle.bundle_id" \
+        node="if node.feats['Aspect']=='Freq': node.feats['Aspect']='Iter'" \
+    > ../hu-ud-$a.conllu
+done
+
+cat ../hu-ud-{train,dev,test}.conllu | udapy -HAMC ud.MarkBugs > bugs.html
