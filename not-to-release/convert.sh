57d7eed24f5fe810a30d46602b45029679668383
Martin Popel <popel@ufal.mff.cuni.cz>
2017-02-24 15:20:33 +0100

    data resplit by @vinczev

--- not-to-release/convert.sh
+++ not-to-release/convert.sh
@@ -4,7 +4,7 @@
 # see http://universaldependencies.org/u/feat/Aspect.html#Iter
 
 for a in train dev test; do
-    cat hu-ud-$a.conllu | udapy -s ud.SetSpaceAfter ud.Convert1to2 util.Eval \
+    cat hu-ud-${a}_resplit.conllu | udapy -s ud.SetSpaceAfter ud.Convert1to2 util.Eval \
         tree="tree.bundle.bundle_id='$a-'+tree.bundle.bundle_id" \
         node="if node.feats['Aspect']=='Freq': node.feats['Aspect']='Iter'" \
     > ../hu-ud-$a.conllu
